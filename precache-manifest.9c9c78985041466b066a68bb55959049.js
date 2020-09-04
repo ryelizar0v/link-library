27982b4f518059fd78e55cbc35d177083a76d837
Rafail Yelizarov <ryelizarov@yandex.com>
2020-09-04 17:41:57 +0800

    Updates

---
+++ precache-manifest.9c9c78985041466b066a68bb55959049.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d05138a9a9dedd82bafc84217315202",
+    "url": "/link-library/index.html"
+  },
+  {
+    "revision": "6b04493fc00fe650b899",
+    "url": "/link-library/static/js/2.307b8693.chunk.js"
+  },
+  {
+    "revision": "fbdad7f2d43bd871e24e7053a49a4902",
+    "url": "/link-library/static/js/2.307b8693.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08ce5a5c3eef80a88e30",
+    "url": "/link-library/static/js/main.c0ab57c5.chunk.js"
+  },
+  {
+    "revision": "83fdd77ff2d854bde098",
+    "url": "/link-library/static/js/runtime-main.8da8fee4.js"
+  }
+]);
