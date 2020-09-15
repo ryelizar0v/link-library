360aa95187ec4faa44c5096a8af5f3b0c1980217
Rafail Yelizarov <ryelizarov@yandex.com>
2020-09-15 18:07:35 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/link-library/precache-manifest.95a066504a2304ebe92702dfafb5f681.js"
+  "/link-library/precache-manifest.aec187b0a5dd12adb1b483759c812ac6.js"
 );
 
 self.addEventListener('message', (event) => {
